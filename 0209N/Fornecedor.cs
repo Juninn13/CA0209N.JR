ea83b3b7bb69dc4e53f1c2f3209b002739fc0678
unknown <lab1060@etecjk.local>
2025-09-10 20:13:15 -0300

    adicionado forncedor

--- 0209N/Fornecedor.cs
+++ 0209N/Fornecedor.cs
@@ -8,5 +8,29 @@ namespace _0209N
 {
     internal class Fornecedor
     {
+        public string nome;
+        public string cnpj;
+        public string codigo;
+
+
+
+        public void cadastroFornecedor()
+        {
+            Console.WriteLine("Digite o nome do Fornecedor:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CNPJ do Fornecedor:");
+            cnpj = Console.ReadLine();
+
+            Console.WriteLine("Digite o Codigo de Cadastro do Fornecedor:");
+            codigo = Console.ReadLine();
+        }
+        public void Exibir()
+        {
+            Console.WriteLine("nome: {0}", nome);
+            Console.WriteLine("CNPJ : {0}", cnpj);
+            Console.WriteLine("Codigo : {0}", codigo);
+        }
     }
 }
+
